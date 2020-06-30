589ac2e1996874393ba8134081cc9d7327b57037
krolik1337 <46852642+krolik1337@users.noreply.github.com>
2020-06-30 15:05:35 +0200

    Added login, registration, different views

--- weather/weather/urls.py
+++ weather/weather/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('weatherApp.urls')),
+    path('', include('login.urls')),
+    path('', include('django.contrib.auth.urls'))
 ]
--- weather/weatherApp/views.py
+++ weather/weatherApp/views.py
@@ -5,8 +5,9 @@ from .forms import CityForm
 
 
 def index(request):
+    flag = False
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=2a5035a1a9287c44491f251ad0719454'
-    extended = Info.objects.first()
+    extended, created = Info.objects.all()[:1].get_or_create(defaults={'extended': False})
     cities = City.objects.all()
     weather_data = []
     ex_weather_data = []
@@ -20,35 +21,47 @@ def index(request):
                 x = City.objects.filter(name=temp).count()
                 cod = requests.get(url.format(temp)).json()['cod']
                 if x == 0 and cod != '404':
-                    form.save()
+                    c = City(name=temp)
+                    c.save()
+                    request.user.city.add(c)
                     message = 'City added.'
-                elif x == 1:
-                    message = 'City already loaded.'
+                elif x != 0:
+                    for c in request.user.city.all():
+                        if temp == c.name:
+                            message = 'City already loaded.'
+                            flag = True
+                    if not flag:
+                        c = City(name=temp)
+                        c.save()
+                        request.user.city.add(c)
+                        message = 'City added.'
                 elif cod == '400':
                     message = 'Invalid city.'
         elif 'info' in request.POST:
             Info.objects.all().update(extended=not extended)
-    extended = Info.objects.first()
-    for city in cities:
-        city_weather = requests.get(url.format(city)).json()
-        weather = {
-            'city': str(city).capitalize(),
-            'temperature': city_weather['main']['temp'],
-            'description': city_weather['weather'][0]['description'],
-            'icon': city_weather['weather'][0]['icon']
-        }
-        weather_data.append(weather)
-        ex_weather = {
-            'city': str(city).capitalize(),
-            'temperature': city_weather['main']['temp'],
-            'feels_like': city_weather['main']['feels_like'],
-            'pressure': city_weather['main']['pressure'],
-            'humidity': city_weather['main']['humidity'],
-            'description': city_weather['weather'][0]['description'],
-            'icon': city_weather['weather'][0]['icon'],
-            'wind_speed': city_weather['wind']['speed']
-        }
-        ex_weather_data.append(ex_weather)
+            extended = Info.objects.all()[:1].get()
+    if request.user.is_authenticated:
+        for city in cities:
+            if city in request.user.city.all():
+                city_weather = requests.get(url.format(city)).json()
+                weather = {
+                    'city': str(city).capitalize(),
+                    'temperature': city_weather['main']['temp'],
+                    'description': city_weather['weather'][0]['description'],
+                    'icon': city_weather['weather'][0]['icon']
+                }
+                weather_data.append(weather)
+                ex_weather = {
+                    'city': str(city).capitalize(),
+                    'temperature': city_weather['main']['temp'],
+                    'feels_like': city_weather['main']['feels_like'],
+                    'pressure': city_weather['main']['pressure'],
+                    'humidity': city_weather['main']['humidity'],
+                    'description': city_weather['weather'][0]['description'],
+                    'icon': city_weather['weather'][0]['icon'],
+                    'wind_speed': city_weather['wind']['speed']
+                }
+                ex_weather_data.append(ex_weather)
     context = {
         'weather_data': weather_data,
         'ex_weather_data': ex_weather_data,
@@ -60,5 +73,5 @@ def index(request):
 
 
 def delete(request, name):
-    City.objects.all().filter(name=name).delete()
+    request.user.city.all().filter(name=name).delete()
     return redirect('main')
